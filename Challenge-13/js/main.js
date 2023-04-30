b6a2bf228cdde2fff4789ce57fbf108430079859
Belen Molina <bm462353@gmail.com>
2023-04-30 19:12:19 -0300

    first commit

---
+++ Challenge-13/js/main.js
@@ -0,0 +1,15 @@
+let saludo1=`hola`;
+let saludo2=`mundo`;
+document.write(`${saludo1} ${saludo2}<br>`);
+
+
+let numero1=parseInt(prompt(`Ingrese el primer numero`));
+let numero2=parseInt(prompt(`Ingrese el segundo numero`));
+let resultadoSuma= document.write(`El resultado de la suma es: ${numero1+numero2}<br>`);
+let resultadoResta=  document.write(`El resultado de la resta es: ${numero1-numero2}<br>`);
+let resultadoMultiplicacion= document.write(`El resultado de la multiplicación es: ${numero1*numero2}<br>`);
+
+
+5
+
+
